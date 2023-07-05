36f147a982896444588aa78c6c15fe4082e3fa8f
lucky2588 <duwcsthang2001@gmail.com>
2023-07-05 23:55:26 +0700

    Create EmailRepository.java

---
+++ Project Web Ecommerce/src/main/java/com/total/webecommerce/respository/OfAdmin/EmailRepository.java
@@ -0,0 +1,7 @@
+package com.total.webecommerce.respository.OfAdmin;
+
+import com.total.webecommerce.entity.Email;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmailRepository extends JpaRepository<Email, Integer> {
+}
