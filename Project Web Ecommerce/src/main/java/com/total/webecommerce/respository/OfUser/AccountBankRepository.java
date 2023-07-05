eb0ef237f3e7c5c36e27a7beb078a3e900db0887
lucky2588 <duwcsthang2001@gmail.com>
2023-07-05 23:49:13 +0700

    Create AccountBankRepository.java

---
+++ Project Web Ecommerce/src/main/java/com/total/webecommerce/respository/OfUser/AccountBankRepository.java
@@ -0,0 +1,7 @@
+package com.total.webecommerce.respository.OfUser;
+
+import com.total.webecommerce.entity.AccountBank;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountBankRepository extends JpaRepository<AccountBank, Integer> {
+}
