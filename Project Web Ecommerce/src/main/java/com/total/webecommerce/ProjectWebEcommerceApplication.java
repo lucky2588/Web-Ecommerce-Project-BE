96acd822ceebb8aadfc01811d8495beafd2866cf
lucky2588 <duwcsthang2001@gmail.com>
2023-07-05 23:57:05 +0700

    Create ProjectWebEcommerceApplication.java

---
+++ Project Web Ecommerce/src/main/java/com/total/webecommerce/ProjectWebEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.total.webecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectWebEcommerceApplication.class, args);
+    }
+
+}
