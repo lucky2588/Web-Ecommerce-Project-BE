38e5e529a29df6a27ed832f945444182e4ce2e2c
lucky2588 <duwcsthang2001@gmail.com>
2023-07-05 23:52:44 +0700

    Create TotalPaymentInfo.java

---
+++ Project Web Ecommerce/src/main/java/com/total/webecommerce/entity/projection/OfUser/TotalPaymentInfo.java
@@ -0,0 +1,10 @@
+package com.total.webecommerce.entity.projection.OfUser;
+
+import com.total.webecommerce.entity.dto.TotalPayment;
+
+/**
+ * Projection for {@link TotalPayment}
+ */
+public interface TotalPaymentInfo {
+    Double getPrice();
+}
