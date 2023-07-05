f7c1440c3187058b67c27a848d4af7aaf69e6aa8
lucky2588 <duwcsthang2001@gmail.com>
2023-07-05 23:52:59 +0700

    Create Role.java

---
+++ Project Web Ecommerce/src/main/java/com/total/webecommerce/entity/Role.java
@@ -0,0 +1,25 @@
+package com.total.webecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "role")
+public class Role implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+
+}
