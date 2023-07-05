a87a87bf16baeb962e26a146e0a66b3c9850b142
lucky2588 <duwcsthang2001@gmail.com>
2023-07-05 23:55:05 +0700

    Create User.java

---
+++ Project Web Ecommerce/src/main/java/com/total/webecommerce/entity/User.java
@@ -0,0 +1,104 @@
+package com.total.webecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User implements Serializable , UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name ="avatar")
+    private String avatar;
+    @Column(name = "isEnable")
+    private Boolean isEnable;
+    @Column(name ="createAt")
+    private LocalDateTime createAt;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Set<Token> tokens = new LinkedHashSet<>();
+    //    @JsonIgnore
+    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.DETACH)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "roles_id"))
+    private List<Role> roles = new ArrayList<>();
+    @PrePersist
+    public void PrePersits(){
+        this.isEnable = false;
+        this.createAt = LocalDateTime.now();
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        this.roles.forEach(role -> {
+            SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" +role.getName());
+            authorities.add(authority);
+        });
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isEnable;
+    }
+
+
+    @PreRemove
+    void PreRemove(){
+        for (Role role : this.roles){
+            role = null;
+        }
+    }
+
+
+
+}
