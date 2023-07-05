dc3ee311c26ac6be757e676769da8e07867a6d3d
lucky2588 <duwcsthang2001@gmail.com>
2023-07-05 23:57:14 +0700

    Create FileResponse.java

---
+++ Project Web Ecommerce/src/main/java/com/total/webecommerce/response/FileResponse.java
@@ -0,0 +1,12 @@
+package com.total.webecommerce.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class FileResponse {
+    private String url;
+}
