be2a328faf6180ef93662e36cfda28e85db63d9e
lucky2588 <duwcsthang2001@gmail.com>
2023-07-05 23:58:18 +0700

    Create CommentProductResquest.java

---
+++ Project Web Ecommerce/src/main/java/com/total/webecommerce/resquest/OfProduct/CommentProductResquest.java
@@ -0,0 +1,17 @@
+package com.total.webecommerce.resquest.OfProduct;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class CommentProductResquest {
+
+    private String email;
+    @NotNull(message = "Không được để trống bình luận  ")
+    private String contentComment;
+}
