e71a409e32ed8b84892b3e4dd1cfc78269758244
lucky2588 <duwcsthang2001@gmail.com>
2023-07-05 23:49:44 +0700

    Create OrderItem.java

---
+++ Project Web Ecommerce/src/main/java/com/total/webecommerce/entity/OrderItem.java
@@ -0,0 +1,52 @@
+package com.total.webecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.apache.catalina.mapper.Mapper;
+import org.springframework.data.jpa.repository.Modifying;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Column(name = "nums")
+    private Integer nums;
+    @Column(name = "price")
+    private Double price;
+    @ManyToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "product_id")
+    private Product product;
+    @PrePersist
+    public void PrePersist(){
+        this.price = (double) (this.product.getPrice() * nums);
+        if(product.getDiscount() != null){
+            this.price = (double ) (this.product.getSales() * nums);
+        }
+    }
+
+    @Modifying
+    @PreUpdate
+    public void PreUpdate(){
+        this.price =(double) (this.product.getPrice() * nums);
+        if(product.getDiscount() != null){
+            this.price = (double ) (this.product.getSales() * nums);
+        }
+    }
+    @PreRemove
+    public void PreRemove(){
+        this.setProduct(null);
+    }
+}
