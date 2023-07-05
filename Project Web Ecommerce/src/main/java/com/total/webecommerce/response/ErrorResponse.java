aa673d511034dfbe4d0843dcdce4ff9b3a93156b
lucky2588 <duwcsthang2001@gmail.com>
2023-07-05 23:57:56 +0700

    Create ErrorResponse.java

---
+++ Project Web Ecommerce/src/main/java/com/total/webecommerce/response/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.total.webecommerce.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+    private HttpStatus status;
+    private Object message;
+}
