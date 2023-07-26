7a34c4e1389442ad927d744b98d72c64c5bb41ef
lucky2588 <duwcsthang2001@gmail.com>
2023-07-26 16:15:25 +0700

    Create ImageProductRepository.java

---
+++ Project Web Ecommerce/src/main/java/com/total/webecommerce/respository/OfProduct/ImageProductRepository.java
@@ -0,0 +1,7 @@
+package com.total.webecommerce.respository.OfProduct;
+
+import com.total.webecommerce.entity.ImageProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageProductRepository extends JpaRepository<ImageProduct, Integer> {
+}
